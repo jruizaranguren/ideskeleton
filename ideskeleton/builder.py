ff011f280f6f6aaf74dd2f4ff3cdfb3831aa147c
jruizaranguren <jruizaranguren@gmail.com>
2015-09-23 13:59:35 +0200

    Make the first test pass by checking if source_path argument exists

--- ideskeleton/builder.py
+++ ideskeleton/builder.py
@@ -1,2 +1,11 @@
-﻿def build(source_path, overwrite = True):
-    pass
+﻿import os.path
+def build(source_path, overwrite = True):
+    if not os.path.exists(source_path):
+        raise IOError("source_path does not exist so not skeleton can be built")
+    '''
+    for root, dirs, files in os.walk("."):
+    path = root.split('/')
+    print (len(path) - 1) *'---' , os.path.basename(root)       
+    for file in files:
+        print len(path)*'---', file
+    '''
