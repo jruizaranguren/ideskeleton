4b150bfeeeb95c35851175a582010aa91b67888e
jruizaranguren <jruizaranguren@gmail.com>
2015-09-24 13:12:55 +0200

    add .git, .cache, .vs to gitignore and take notes on walking dir structure

--- ideskeleton/builder.py
+++ ideskeleton/builder.py
@@ -9,3 +9,28 @@ def build(source_path, overwrite = True, ide = "vstudio"):
     for file in files:
         print len(path)*'---', file
     '''
+
+    """ Read .gitignore if existing. Fail if not existing.
+
+    [to_ignore]
+    with open(".gitignore") as fgit:
+    for line in fgit:
+        trimmed = line.strip()
+        if trimmed and not trimmed.startswith("#"):
+            to_ignore.append(trimmed)
+    """
+
+    """ return if it is ignored
+    def is_ignored(path):
+    for pattern in to_ignore:
+        if fnmatch.fnmatch(path,pattern):
+            return True
+    return False
+    """
+
+    """ Recursive filtering
+    for root, dirs, files in os.walk("."):
+    dirs = [d for d in dirs if not is_ignored(d)]
+    files = [f for f in files if not is_ignored(f)]
+    print root, dirs, files
+    """
