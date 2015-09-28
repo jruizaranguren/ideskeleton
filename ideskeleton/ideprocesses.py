dd5563c8fdab3ac53a6f53f3f61c5bd8f4f736f7
jruizaranguren <jruizaranguren@gmail.com>
2015-09-28 16:15:59 +0200

    start implementing vstudio_read, creates solution file

--- ideskeleton/ideprocesses.py
+++ ideskeleton/ideprocesses.py
@@ -1,3 +1,5 @@
+﻿
+from os.path import abspath, basename
 
 def none_read(level, root, dirs, files):
     return [(level, root, dirs, files)]
@@ -5,8 +7,14 @@ def none_read(level, root, dirs, files):
 def none_write(actions, path):
     return actions
 
+def vstudio_read(level, root, dirs, files):
+    next_actions = []
+    solution_name = basename(abspath(root))
+    next_actions.append(("add_solution",solution_name))
+    return next_actions
+
 
 processes = {
-        "vstudio": [None, None],
+        "vstudio": [vstudio_read, None],
         None : [none_read, none_write]
     }
--- tests/test_ideprocesses.py
+++ tests/test_ideprocesses.py
@@ -1,4 +1,5 @@
-import pytest
+﻿import pytest
+import os.path
 from ideskeleton import ideprocesses as ide
 
 def test_none_read_it_just_returns_the_input_within_a_list():
@@ -10,6 +11,18 @@ def test_none_write_returns_input_ignoring_path():
     actual = ide.none_write([],"C:/projects")
 
     assert actual == []
+
+def test_vstudio_read_add_files_at_level_zero_to_solution():
+    
+    pass
+
+def test_vstudio_a_solution_is_created_at_level_zero_for_relative_paths():
+    actual =  ide.vstudio_read(0,".", [],[])
+    assert actual[0] == ("add_solution", "ideskeleton")
+
+def test_vstudio_a_solution_is_created_at_level_zero_for_absolute_paths():
+    actual = ide.vstudio_read(0,"C:/Projects/MyProject",[],[])
+    assert actual[0] == ("add_solution", "MyProject")
 
 
 '''
