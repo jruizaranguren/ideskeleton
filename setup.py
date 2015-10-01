4eb7384f7cdef6e6e7d8b7c3917ff720445cae0e
Javier Ruiz Aranguren <jruizaranguren@gmail.com>
2015-10-01 19:11:34 +0200

    use the readme.rst as long_description for the package distribution

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 ﻿from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='ideskeleton',
       version='0.10',
       description="Scaffolding of IDE project files such as Visual Studio from Python existing" \
           "folder structure",
+      long_description=readme(),
       author="Javier Ruiz Aranguren",
       license="MIT",
       url="http://github.com/jruizaranguren/ideskeleton",
