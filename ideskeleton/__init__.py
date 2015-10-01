46fcd2634ec084f5fbc300155b77034ed0da4799
jruizaranguren <jruizaranguren@gmail.com>
2015-10-01 10:06:15 +0200

    Resolve pylint quibbles in __init__.py, __main__.py and builder.py

--- ideskeleton/__init__.py
+++ ideskeleton/__init__.py
@@ -1,1 +1,14 @@
-﻿from .builder import build
+﻿"""
+ideskeleton
+=====
+Build required files to work with IDE's such as solution and project files.
+-----------------
+
+Notes
+-----
+Currently just Visual Studio is supported.
+
+
+"""
+from .builder import build
+
--- ideskeleton/__main__.py
+++ ideskeleton/__main__.py
@@ -1,30 +1,28 @@
-﻿"""Build IDE required files from python folder structure
-
-This utility creates the required files needed to manage a python
- package or module for a particular IDE from folder structure.
-
-Note:
-    Currently only Visual Studio 2015 and python projects are supported
-
+﻿"""Build IDE required files from python folder structure from command line
 """
 import argparse
 from ideskeleton.builder import build
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("source_path",
-                        help="root path of the folder structure used to generate the IDE skeleton",
-                        type=str)
-    parser.add_argument("-f",
-                        "--force",
-                        help="force overwrite existing solution and project files",
-                        action="store_true")
-    parser.add_argument("-i",
-                        "--ide",
-                        help="choose IDE",
-                        type=str,
-                        choices=["vstudio"])
+    """Build IDE files from python folder structure."""
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "source_path",
+        help="path of the folder structure used to generate the IDE skeleton",
+        type=str)
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="force overwrite existing solution and project files",
+        action="store_true")
+    parser.add_argument(
+        "-i",
+        "--ide",
+        help="choose IDE",
+        type=str,
+        choices=["vstudio"])
 
     args = parser.parse_args()
 
