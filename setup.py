5957d70b89098df1498a1c8d925117d9afb0b9fe
jruizaranguren <jruizaranguren@gmail.com>
2015-10-01 17:00:04 +0200

    add setup.py to distribute package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ideskeleton',
+      version='0.10',
+      description="Scaffolding of IDE project files such as Visual Studio from Python existing" \
+          "folder structure",
+      author="Javier Ruiz Aranguren",
+      packages=['ideskeleton']
+      )
+
+__author__ = "Javier Ruiz Aranguren"
